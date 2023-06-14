21abfe4ea31b4fe66b733f9680bffa16c7ccc47e
Mekha L <mekhalnair@gmail.com>
2023-06-14 16:21:36 +0530

    Update bankers.c

--- Week 7/bankers.c
+++ Week 7/bankers.c
@@ -1,80 +1,92 @@
-// Banker's Algorithm
-#include <stdio.h>
-int main()
-{
-	// P0, P1, P2, P3, P4 are the Process names here
+ #include <stdio.h>
 
-	int n, m, i, j, k;
-	n = 5; // Number of processes
-	m = 3; // Number of resources
-	int alloc[5][3] = { { 0, 1, 0 }, // P0 // Allocation Matrix
-						{ 2, 0, 0 }, // P1
-						{ 3, 0, 2 }, // P2
-						{ 2, 1, 1 }, // P3
-						{ 0, 0, 2 } }; // P4
+#define MAX_PROCESSES 10
+#define MAX_RESOURCES 10
 
-	int max[5][3] = { { 7, 5, 3 }, // P0 // MAX Matrix
-					{ 3, 2, 2 }, // P1
-					{ 9, 0, 2 }, // P2
-					{ 2, 2, 2 }, // P3
-					{ 4, 3, 3 } }; // P4
+int main() {
+    int available[MAX_RESOURCES];
+    int maximum[MAX_PROCESSES][MAX_RESOURCES];
+    int allocation[MAX_PROCESSES][MAX_RESOURCES];
+    int need[MAX_PROCESSES][MAX_RESOURCES];
+    int work[MAX_RESOURCES];
+    int finish[MAX_PROCESSES] = {0};
 
-	int avail[3] = { 3, 3, 2 }; // Available Resources
+    int numProcesses, numResources;
 
-	int f[n], ans[n], ind = 0;
-	for (k = 0; k < n; k++) {
-		f[k] = 0;
-	}
-	int need[n][m];
-	for (i = 0; i < n; i++) {
-		for (j = 0; j < m; j++)
-			need[i][j] = max[i][j] - alloc[i][j];
-	}
-	int y = 0;
-	for (k = 0; k < 5; k++) {
-		for (i = 0; i < n; i++) {
-			if (f[i] == 0) {
+    printf("Enter the number of processes: ");
+    scanf("%d", &numProcesses);
 
-				int flag = 0;
-				for (j = 0; j < m; j++) {
-					if (need[i][j] > avail[j]){
-						flag = 1;
-						break;
-					}
-				}
+    printf("Enter the number of resources: ");
+    scanf("%d", &numResources);
 
-				if (flag == 0) {
-					ans[ind++] = i;
-					for (y = 0; y < m; y++)
-						avail[y] += alloc[i][y];
-					f[i] = 1;
-				}
-			}
-		}
-	}
+    // Input the available resources
+    printf("Enter the available resources:\n");
+    for (int i = 0; i < numResources; i++) {
+        printf("Resource %d: ", i);
+        scanf("%d", &available[i]);
+    }
 
-	int flag = 1;
-	
-	for(int i=0;i<n;i++)
-	{
-	if(f[i]==0)
-	{
-		flag=0;
-		printf("The following system is not safe");
-		break;
-	}
-	}
-	
-	if(flag==1)
-	{
-	printf("Following is the SAFE Sequence\n");
-	for (i = 0; i < n - 1; i++)
-		printf(" P%d ->", ans[i]);
-	printf(" P%d", ans[n - 1]);
-	}
-	
+    // Input the maximum resource allocation for each process
+    printf("Enter the maximum resource allocation for each process:\n");
+    for (int i = 0; i < numProcesses; i++) {
+        printf("Process %d:\n", i);
+        for (int j = 0; j < numResources; j++) {
+            printf("Resource %d: ", j);
+            scanf("%d", &maximum[i][j]);
+        }
+    }
 
-	return (0);
+    // Input the current resource allocation for each process
+    printf("Enter the current resource allocation for each process:\n");
+    for (int i = 0; i < numProcesses; i++) {
+        printf("Process %d:\n", i);
+        for (int j = 0; j < numResources; j++) {
+            printf("Resource %d: ", j);
+            scanf("%d", &allocation[i][j]);
 
-	// This code is contributed by Deep Baldha (CandyZack)
+            // Calculate the need matrix
+            need[i][j] = maximum[i][j] - allocation[i][j];
+        }
+    }
+
+    // Initialize work and finish arrays
+    for (int i = 0; i < numResources; i++) {
+        work[i] = available[i];
+    }
+
+    // Perform the Banker's Algorithm
+    int count = 0;
+    while (count < numProcesses) {
+        int found = 0;
+        for (int i = 0; i < numProcesses; i++) {
+            if (finish[i] == 0) {
+                int j;
+                for (j = 0; j < numResources; j++) {
+                    if (need[i][j] > work[j]) {
+                        break;
+                    }
+                }
+                if (j == numResources) {
+                    for (int k = 0; k < numResources; k++) {
+                        work[k] += allocation[i][k];
+                    }
+                    finish[i] = 1;
+                    found = 1;
+                    count++;
+                }
+            }
+        }
+        if (found == 0) {
+            break;  // System is in an unsafe state
+        }
+    }
+
+    // Check if the system is in a safe state
+    if (count == numProcesses) {
+        printf("The system is in a safe state.\n");
+    } else {
+        printf("The system is in an unsafe state.\n");
+    }
+
+    return 0;
 }
